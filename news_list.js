be243526fd47636de2842d9101e5f68b9a6d0655
lingz <zhangling.me@foxmail.com>
2021-10-10 00:32:15 +0800

    update

--- news_list.js
+++ news_list.js
@@ -10,8 +10,12 @@ $('.news_list_ul li span').each((i, element) => {
   let text = $(element).text()
   // if (text.includes('晨和公寓')) {
   if (text.includes('国庆放假')) {
-    // 推送
+    // 推送，todo 接入WxPusher
     console.log(text)
 
   }
 });
+
+// todo 定时任务，每天9:00跑（网站开放登录时间为9点）。若命中关键词推送/全部推送
+// todo 登录，查看当前排队信息。若信息变化推送/全部推送
+// todo 捡漏推送
