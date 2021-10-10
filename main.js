a45950734a4ec27c48d3f3a2a1dc43e1b5711d16
lingz <zhangling.me@foxmail.com>
2021-10-10 23:54:00 +0800

    定时任务schedule接入

---
+++ main.js
@@ -0,0 +1,12 @@
+const schedule = require('./lib/schedule')
+const news_list = require('./news_list')
+
+/**
+ * 入口文件（主逻辑）
+ */
+
+// 每天9点推送公告·关键词过滤结果
+// 每天9:00跑job原因（网站开放登录时间为9点）
+// const cron = '0 0 9 * * *'
+const cron = '* * * * * *'
+schedule(cron, news_list)
--- news_list.js
+++ news_list.js
@@ -4,17 +4,19 @@ const request = require("sync-request")
 /**
  * 公告列表 关键词过滤并推送
  */
-const body = request('GET', 'https://shcngz.com/pages/news_list.aspx?mid=29').getBody().toString()
-let $ = cheerio.load(body)
-$('.news_list_ul li span').each((i, element) => {
-  let text = $(element).text()
-  // if (text.includes('晨和公寓')) {
-  if (text.includes('国庆放假')) {
-    // 推送，todo 接入WxPusher
-    console.log(text)
+module.exports = function() {
+  const body = request('GET', 'https://shcngz.com/pages/news_list.aspx?mid=29').getBody().toString()
+  let $ = cheerio.load(body)
+  $('.news_list_ul li span').each((i, element) => {
+    let text = $(element).text()
+    if (text.includes('晨和公寓')) {
+    // if (text.includes('国庆放假')) {
+      // 推送，todo 接入WxPusher
+      console.log(text)
 
-  }
-});
+    }
+  });
+}
 
 // todo 定时任务，每天9:00跑（网站开放登录时间为9点）。若命中关键词推送/全部推送
 // todo 登录，查看当前排队信息。若信息变化推送/全部推送
